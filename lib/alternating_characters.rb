002438dbf88fb7225aa04f946c516fe1463dbd2f
Justin Thomas <jthomas0209@gmail.com>
2016-06-03 17:45:01 -0400

    added new problem, need to fix test check for input

---
+++ lib/alternating_characters.rb
@@ -0,0 +1,32 @@
+
+class AlternatingCharacters
+
+  def self.run
+    puts "Enter the number of test cases"
+    num_cases = gets.chomp.to_i
+    
+    for i in (1..num_cases)
+      puts "Enter the AB string sequence"
+      sequence = gets.chomp.split("")
+      self.alternate_check(sequence)
+    end
+  end
+
+  def self.alternate_check(sequence)
+    i = 0
+    j = 1
+    num_deletions = 0;
+    while sequence.length > 1 && j != sequence.length
+      if sequence[i] == sequence[j]
+        sequence.delete_at(j)
+        num_deletions += 1
+      else
+        i += 1
+        j += 1
+      end
+    end
+    puts num_deletions
+  end
+
+  AlternatingCharacters.run 
+end
---
+++ spec/alternating_characters_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+require 'stringio'
+
+describe AlternatingCharacters do
+  describe '#run' do
+    it 'should return the number of deletions' do
+      
+    end 
+  end
+end
