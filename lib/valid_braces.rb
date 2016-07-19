90437f4fbd4b8be6b500b215eb6bc12bc7ac4e35
Justin Thomas <jthomas0209@gmail.com>
2016-07-19 15:07:42 -0400

    removed string interpolation, not needed with raw strings

--- lib/valid_braces.rb
+++ lib/valid_braces.rb
@@ -7,9 +7,9 @@ def valid_braces(braces)
   return false if brace_pairs.keys.include?(brace_array[0].to_sym) || brace_pairs.values.include?(brace_array[end_index])
   
   brace_array.each do |brace|
-    char_stack << brace if (char_stack.length == 0 || brace_pairs.values.include?("#{brace}"))
-    if brace_pairs.keys.include?("#{brace}".to_sym)
-      return false if char_stack.last != brace_pairs["#{brace}".to_sym]
+    char_stack << brace if (char_stack.length == 0 || brace_pairs.values.include?(brace))
+    if brace_pairs.keys.include?(brace.to_sym)
+      return false if char_stack.last != brace_pairs[brace.to_sym]
       char_stack.pop
     end
   end
